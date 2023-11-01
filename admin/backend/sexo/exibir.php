b739aa8effcdb1325751e1de7f19dabf6326afd8
Gustavo Rizerio <GustavoGrizerioG@gmail.com>
2023-11-01 16:41:48 -0300

    arrumando a ordem da pagina de adm

--- admin/backend/sexo/exibir.php
+++ admin/backend/sexo/exibir.php
@@ -4,9 +4,9 @@ include_once('../../../config/config.php');
 
 if (!empty($_GET['search'])) {
     $data = $_GET['search'];
-    $sql = "SELECT * FROM sexo WHERE id LIKE '%$data%' or sexo LIKE '%$data%' ORDER BY id DESC";
+    $sql = "SELECT * FROM sexo WHERE id LIKE '%$data%' or sexo LIKE '%$data%' ORDER BY id";
 } else {
-    $sql = "SELECT * FROM sexo ORDER BY id DESC";
+    $sql = "SELECT * FROM sexo ORDER BY id";
 }
 
 $result = $conexao->query($sql);
--- admin/backend/subcategoria/exibir.php
+++ admin/backend/subcategoria/exibir.php
@@ -3,9 +3,9 @@ include_once('../../../config/config.php');
 
 if (!empty($_GET['search'])) {
     $data = $_GET['search'];
-    $sql = "SELECT * FROM subcategoria WHERE id LIKE '%$data%' or nome_subcatg LIKE '%$data%' ORDER BY id DESC";
+    $sql = "SELECT * FROM subcategoria WHERE id LIKE '%$data%' or nome_subcatg LIKE '%$data%' ORDER BY id";
 } else {
-    $sql = "SELECT * FROM subcategoria ORDER BY id DESC";
+    $sql = "SELECT * FROM subcategoria ORDER BY id";
 }
 
 $result = $conexao->query($sql);
